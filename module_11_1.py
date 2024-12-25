fd622b94b795f1841230cb32d535d857cccbe0ca
Ilya-23 <ilya.gawrin.7635@gmail.com>
2024-12-25 21:27:58 +0700

    Add files via upload

---
+++ module_11_1.py
@@ -0,0 +1,23 @@
+#matplotlib
+import matplotlib.pyplot as plt
+x = [1, 3, 5, 7, 9]
+y = [2, 6, 3, 8, 15]
+plt.plot(x, y)
+plt.xlabel('ось X')
+plt.ylabel('ось Y')
+plt.title('График')
+plt.show()
+
+#NumPy
+import numpy as np
+a = np.empty((3,3))
+b = np.empty((3,3))
+print(a)
+print(b)
+print(a.shape)
+print(a-b)
+
+
+
+
+
